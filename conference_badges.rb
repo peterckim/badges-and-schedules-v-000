fa8d0846a2a8d4b704934dd09d3af918232a8a0a
Peter Kim <peterckim96@gmail.com>
2018-06-01 18:05:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,8 @@ def batch_badge_creator(names)
   return list
 end
 
-def assign_rooms(names)
-  list = []
-  count = 0
-  names.each do |name|
+def assign_rooms(list)
+  list.each_with_index do |phrase|
     list[count] = "Hello, #{name}! You'll be assigned to room #{count + 1}!"
 end
 
