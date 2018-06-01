1d98c7f2378c00087e05139abebe3332ef4391ab
Peter Kim <peterckim96@gmail.com>
2018-06-01 18:00:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  
+end
+
+def assign_rooms(names)
+  
+end
+
+def printer
+  
+end
